dc934268a38fe135d04b5d2f10f1b9c78d2e57cd
yoqub11 <isayevyoqub0@gmail.com>
2026-06-07 16:44:15 +0500

    Create CrmApplication.java

---
+++ src/main/java/com/company/crm/CrmApplication.java
@@ -0,0 +1,11 @@
+package com.company.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApplication.class, args);
+    }
+}
